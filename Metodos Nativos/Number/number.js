4c5df4fa9ed929595e472fef2d97acfa056d395f
gioinsfran1998 <gioinsfran98@gmail.com>
2019-04-24 09:15:13 -0400

    Numbers

---
+++ Metodos Nativos/Number/number.js
@@ -0,0 +1,12 @@
+console.log(Number.isNaN(50));
+console.log(Number.isNaN(0/0));
+
+console.log(Number.isFinite(50));
+
+console.log(Number.isInteger(50));
+
+console.log(Number.EPSILON);
+
+console.log(Math.trunc(3.1416));
+
+console.log(Math.sign(7));
